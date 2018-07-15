62b8205e33984920bdf49c481d01ed49c6784ed2
Carlos Martin <cjmar87@gmail.com>
2018-07-15 14:43:38 -0500

    starting

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const path = require('path');
+
+const express = require('express');
+const socketio = require('socket.io');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio.listen(server);
+
+require('./sockets')(io);
+
+app.use(express.static(path.join(__dirname,'public')));
+
+server.listen(3000, () => {
+    console.log('server listening on port');
+});
