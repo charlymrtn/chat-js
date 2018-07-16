18742736e2ab1b9873f3430ce872113926ce724a
Carlos Martin <cjmar87@gmail.com>
2018-07-16 01:48:38 -0500

    iniciamos db

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -31,7 +31,9 @@ $(function (){
 
     $messageForm.submit(e => {
         e.preventDefault();
-        socket.emit('send message', $messageBox.val());
+        socket.emit('send message', $messageBox.val(), data =>{
+            $chat.append(`<p class="error">${data}</p>`)
+        });
         $messageBox.val('');
         
     });
@@ -46,5 +48,9 @@ $(function (){
             html += `<p><i class="fas fa-user"></i>${data[i]}</p>`;
         }
         $usernames.html(html);
+    });
+
+    socket.on('whisper', (data)=>{
+        $chat.append(`<p class="whisper"><b>${data.nick}:</b> ${data.msg}</p>`);
     });
 });
--- src/sockets.js
+++ src/sockets.js
@@ -1,36 +1,58 @@
 module.exports = (io) =>{
 
-    let nicknames = [];
+    let users = {};
 
     io.on('connection', socket => {
         console.log('new user connected');
 
         socket.on('new user', (data, cb) =>{
-            if (nicknames.indexOf(data) != -1) {
+            if (data in users) {
                 cb(false);
             }else{
                 cb(true);
                 socket.nickname = data;
-                nicknames.push(socket.nickname);
+                users[socket.nickname] = socket;
                 updateNicks();
             }
         });
 
-        socket.on('send message',(data)=>{
-            io.sockets.emit('new message', {
-                msg: data,
-                nick: socket.nickname
-            });
+        socket.on('send message',(data, cb)=>{
+
+            var msg = data.trim();
+            if (msg.substr(0, 3) === '/w ') {
+                msg = msg.substr(3);
+                const index = msg.indexOf(' ');
+                if (index != -1) {
+                    var name = msg.substr(0, index);
+                    var msg = msg.substr(index + 1);
+
+                    if (name in users) {
+                        users[name].emit('whisper', {
+                            msg,
+                            nick: socket.nickname
+                        });
+                    }else{
+                        cb('Error! ingresa un usuario validado');
+                    }
+                } else {
+                    cb('ERROR por favor ingresa tu mensaje');
+                }
+            }else{
+                io.sockets.emit('new message', {
+                    msg: data,
+                    nick: socket.nickname
+                });
+            } 
         });
 
         socket.on('disconnect', data =>{
             if (socket.nickname) return;
-            nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+            delete users[socket.nickname];
             updateNicks();
         });
 
         function updateNicks(){
-            io.sockets.emit('usernames', nicknames);
+            io.sockets.emit('usernames', Object.keys(users));
         }
     });
 };
